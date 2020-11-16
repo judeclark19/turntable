fa85a022d129d9b96a5650adcf1f433707d38b64
Jude Clark <judeclark2019@gmail.com>
2020-11-15 19:23:28 -0500

    Ed's custom scrollbar css

--- src/components/LibraryItem.jsx
+++ src/components/LibraryItem.jsx
@@ -4,8 +4,10 @@ function LibraryItem({ song }) {
   return (
     <div className="library-item">
       <img src={song.cover} alt={song.name} />
-      <h3>{song.name}</h3>
-      <h4>{song.artist}</h4>
+      <div className="song-info">
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
     </div>
   );
 }
